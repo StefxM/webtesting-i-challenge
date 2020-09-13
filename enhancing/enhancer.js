ff94ed411ec0af82db3d90702c622a0a9618865a
stephanie mairena <stefyem7711@gmail.com>
2020-09-12 23:05:48 -0400

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,35 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+  return { ...item, enhancement: item.enhancement + 1, };
+  } else if (item.enhancement >= 20) 
+    return {...item, enhancement: 20}
 }
 
 function fail(item) {
+  if (item.enhancement < 15) { 
+    return {...item, enhancement: item.enhancement - 5}
+  }
+  else if (item.enhancement > 16) {
+    return {...item, enhancement: item.enhancement - 1, durability: item.durability -10}
+  }
+    else if (item.enhancement >= 15) { 
+      return { ...item, durability: item.durability -10}
+    } 
+    
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+
+
+  return { ...item, durability:100 };
 }
 
 function get(item) {
   return { ...item };
 }
+
+
+//helpers
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,41 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+it("Has been repaired", () => { 
+    expect(enhancer.repair({
+        name: "Shoe",
+        durability: 75,
+        enhancement: 18,
+    }))
+    .toEqual({
+        name: "Shoe",
+        durability: 100,
+        enhancement: 18,
+    })
+})
+
+it("Has been succeeded", () => { 
+    expect(enhancer.success({
+        name: "Shoe",
+        durability: 75,
+        enhancement: 18,
+    }))
+    .toEqual({
+        name: "Shoe",
+        durability: 75,
+        enhancement: 19,
+    })
+})
+
+it("Has been failed", () => { 
+    expect(enhancer.fail({
+        name: "Shoe",
+        durability: 75,
+        enhancement: 18,
+    }))
+    .toEqual({
+        name: "Shoe",
+        durability: 65,
+        enhancement: 17,
+    })
+})
